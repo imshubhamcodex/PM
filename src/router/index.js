cbda3ce0d21104037eb8f24230d491973f1dc6cb
imshubhamcodex <42334452+imshubhamcodex@users.noreply.github.com>
2020-06-28 11:37:30 +0530

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import login_signup_user from '../components/login_signup_user.vue';
 import user from '../components/user.vue';
+import auth from '../components/auth.vue';
 
 export default [
 {
 	path:'/',
-	component: login_signup_user
+	component:auth
 },
 {
 	path: '/login_signup_user',
